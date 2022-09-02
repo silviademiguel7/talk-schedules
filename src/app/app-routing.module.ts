93dc55c4afc9e10a85aa122aa507bc6f4f89ca30
U01A39A2 <silvia.demiguel@viewnext.com>
2022-09-02 12:07:28 +0200

    feat: initialize app with its routes and base components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventListComponent } from './features/task-schedules/delivery/event-list/event-list.component';
+import { SchedulesComponent } from './features/task-schedules/delivery/schedules/schedules.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/events', pathMatch: 'full' },
+  { path: 'events', component: EventListComponent },
+  { path: 'events/:id', component: SchedulesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventListComponent } from './features/task-schedules/delivery/event-list/event-list.component';
+import { SchedulesComponent } from './features/task-schedules/delivery/schedules/schedules.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, EventListComponent, SchedulesComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
