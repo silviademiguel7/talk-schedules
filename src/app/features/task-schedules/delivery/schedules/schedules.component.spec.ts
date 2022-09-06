3351098cab0484ad3237e5de3f5ff06b0dc01f71
U01A39A2 <silvia.demiguel@viewnext.com>
2022-09-06 16:55:22 +0200

    test: run all test correctly

--- src/app/features/task-schedules/delivery/schedules/schedules.component.spec.ts
+++ src/app/features/task-schedules/delivery/schedules/schedules.component.spec.ts
@@ -1,13 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SchedulesComponent } from './schedules.component';
+import { ActivatedRoute } from '@angular/router';
 
 describe('SchedulesComponent', () => {
   let component: SchedulesComponent;
   let fixture: ComponentFixture<SchedulesComponent>;
+  const fakeActivatedRoute = {
+    snapshot: { data: {} },
+  } as ActivatedRoute;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SchedulesComponent],
+      providers: [{ provide: ActivatedRoute, useValue: fakeActivatedRoute }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SchedulesComponent);
--- src/app/features/task-schedules/delivery/schedules/schedules.component.ts
+++ src/app/features/task-schedules/delivery/schedules/schedules.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { getEventById } from '../../application/getEventById';
 
